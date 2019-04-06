d3210946a826c00da029ddd5c038314e80dc4eaa
purgeyao <1346735074@qq.com>
2019-04-06 11:56:45 +0800

    new

---
+++ crawler-core/src/main/java/com/crawler/core/test.java
@@ -0,0 +1,9 @@
+package com.crawler.core;
+
+/**
+ * @author purgeyao
+ * @version 1.0
+ * @date 2019-04-06
+ */
+public class test {
+}
---
+++ crawler-demo/src/main/java/com/crawler/demo/SpringBootApp.java
@@ -0,0 +1,17 @@
+package com.crawler.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author purgeyao
+ * @version 1.0
+ * @date 2019-04-06
+ */
+@SpringBootApplication
+public class SpringBootApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
+}
